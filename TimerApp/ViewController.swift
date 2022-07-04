65e2bd16e69ff9ca9d5625b570a91c17d68c32bf
Özge Poyraz <71945096+ozgepoyraz@users.noreply.github.com>
2022-07-04 21:04:34 +0300

    Initial Commit

---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Özge Poyraz on 4.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
